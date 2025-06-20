25570eaaccfe270aaf23347bba2f651514f49024
Furioz <Fu2iosa@gmail.com>
2025-06-20 17:48:56 +0200

    scripts/Custom : Implement m+ affixes into laoder

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -17,10 +17,46 @@
 
 // This is where scripts' loading functions should be declared:
 // void MyExampleScript()
+void AddSC_spell_custom_grievous();           // handles 3001000 DOT
+void AddSC_spell_custom_raging();
+void AddSC_spell_custom_storming();
+void AddSC_spell_custom_volcanic();
+void AddSC_spell_custom_bolstering();
+void AddSC_spell_custom_sanguine();
+void AddSC_spell_custom_explosive();
+void AddSC_spell_custom_quaking();
+void AddSC_spell_custom_spiteful();
+void AddSC_spell_custom_entangling();
+void AddSC_npc_entangling_plant();
+void AddSC_spell_custom_inspiring();
+void AddSC_spell_custom_shielding();
+void AddSC_npc_shielding_orb();
+void AddSC_npc_manifestation_of_pride();
+void AddSC_spell_belligerent_missile_launcher();
+void AddSC_npc_belligerent_missile();
+void AddSC_spell_bursting_with_pride();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
     // MyExampleScript()
+    AddSC_spell_custom_grievous();           // handles 3001000 DOT
+    AddSC_spell_custom_raging();
+    AddSC_spell_custom_storming();
+    AddSC_spell_custom_volcanic();
+    AddSC_spell_custom_bolstering();
+    AddSC_spell_custom_sanguine();
+    AddSC_spell_custom_explosive();
+    AddSC_spell_custom_quaking();
+    AddSC_spell_custom_spiteful();
+    AddSC_spell_custom_entangling();
+    AddSC_npc_entangling_plant();
+    AddSC_spell_custom_inspiring();
+    AddSC_spell_custom_shielding();
+    AddSC_npc_shielding_orb();
+    AddSC_npc_manifestation_of_pride();
+    AddSC_spell_belligerent_missile_launcher();
+    AddSC_npc_belligerent_missile();
+    AddSC_spell_bursting_with_pride();
 }
